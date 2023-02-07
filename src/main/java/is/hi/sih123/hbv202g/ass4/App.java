bef530e170635ff869f27ba8d1f83fe452387e68
Arnaeiriks <are45@hi.is>
2023-02-07 11:37:56 +0000

    prime tala

--- src/main/java/is/hi/sih123/hbv202g/ass4/App.java
+++ src/main/java/is/hi/sih123/hbv202g/ass4/App.java
@@ -1,13 +1,15 @@
 package is.hi.sih123.hbv202g.ass4;
 
+import org.apache.commons.numbers.primes.Primes;
+
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int number = 123456789;
+        int nextPrime = Primes.nextPrime(number);
+        System.out.println("Next number after "+ number+ " is "+nextPrime );
     }
 }
